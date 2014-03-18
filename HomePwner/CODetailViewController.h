5bca7e7a1d40470a20fd3fef1ced7ef21eb9fcea
cokeefe <chaemobile@gmail.com>
2014-03-18 13:34:33 -0400

    Create detail view controller

---
+++ HomePwner/CODetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CODetailViewController.h
+//  HomePwner
+//
+//  Created by Chae O'Keefe on 3/18/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CODetailViewController : UIViewController
+
+@end
---
+++ HomePwner/CODetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  CODetailViewController.m
+//  HomePwner
+//
+//  Created by Chae O'Keefe on 3/18/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import "CODetailViewController.h"
+
+@interface CODetailViewController ()
+
+@end
+
+@implementation CODetailViewController
+
+@end
