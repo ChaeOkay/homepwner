ee848ab19121a6c49fbe1cc196ca4e56016b91d0
cokeefe <chaemobile@gmail.com>
2014-03-18 12:02:06 -0400

    Implement Required Table View Data Source Interface
    * Tell Table View controller how to find cell data

--- HomePwner/COItemsViewController.m
+++ HomePwner/COItemsViewController.m
@@ -7,12 +7,44 @@
 //
 
 #import "COItemsViewController.h"
+#import "COItemStore.h"
+#import "BNRItem.h"
+
+@interface COItemsViewController () <UITableViewDataSource>
+
+@end
 
 @implementation COItemsViewController
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return COItemStore.sharedStore.allItems.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"DolphinsForPretzels"];
+
+    NSArray *items = COItemStore.sharedStore.allItems;
+    BNRItem *item = items[indexPath.row];
+    cell.textLabel.text = item.description;
+
+    return cell;
+}
+
 - (instancetype)init
 {
-    return [super initWithStyle:UITableViewStylePlain];
+    self = [super initWithStyle:UITableViewStylePlain];
+
+    if (self)
+    {
+        for (int i = 0; i < 5; i++){
+            [[COItemStore sharedStore] createItem];
+        }
+    }
+
+    return self;
 }
 
 - (instancetype)initWithStyle:(UITableViewStyle)style
